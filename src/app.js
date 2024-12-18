4b72615f01d7d57f160644cdad8aa8e1b817df5d
Taha <taha100_100@yahoo.com>
2024-12-18 03:41:21 +0200

    Added needed packages for apis secturity

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const hpp = require("hpp");
+const xss = require("xss-clean");
+const cookieParser = require("cookie-parser");
+const compression = require("compression");
 const AppError = require("./util/appError");
 const globalErrorHandler = require("./controllers/v1/errorController");
 const countryRouter = require("./routes/countryRoutes");
@@ -18,9 +24,23 @@ const internationalLicenseRouter = require("./routes/internationalLicenseRoutes"
 
 const app = express();
 app.use(cors());
+const limiter = rateLimit({
+  windowMs: 60 * 60 * 1000,
+  limit: 100,
+  standardHeaders: "draft-7",
+  legacyHeaders: false,
+  message: "Too many requests from this IP, please try again after an hour",
+});
 
 app.use(express.static(path.join(__dirname, "../public")));
 app.use(express.json());
+
+app.use(helmet({ contentSecurityPolicy: false }));
+app.use("/api", limiter);
+app.use(xss());
+app.use(hpp());
+app.use(cookieParser());
+
 app.use("/api/v1/users", userRouter);
 app.use(authController.protect);
 app.use("/api/v1/countries", countryRouter);
@@ -36,6 +56,7 @@ app.use(
   "/api/v1/local_driving_license_applications",
   LocalDrivingLicenseApplicationRouter,
 );
+app.use(compression());
 
 app.all("*", (err, req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
